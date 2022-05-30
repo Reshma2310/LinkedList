38a6f3ab91518c8adf4dc60353acdb04dca76d0b
Reshma2310 <dreshma253@gmail.com>
2022-05-30 15:16:56 +0500

    [Reshma] - UC1- Creating Simple Linked List

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            LinkedListClass list = new LinkedListClass();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
+
+
